adb83c8318d99241b29ba063f7ad0ad958b7055a
李文雪 <liwenxue@xiaomi.com>
2025-10-27 21:34:01 +0800

    m

--- train.py
+++ train.py
@@ -53,6 +53,7 @@ def main():
     config.use_gpu = args.use_gpu
     config.config_path = args.config_path
     config.logdir = args.logdir
+    config.generator_ckpt = args.generator_ckpt
     
     config.seq_len = int(config.height // 16) * int(config.width // 16) * int((config.num_frames - 1) // config.time_division_factor)
     
