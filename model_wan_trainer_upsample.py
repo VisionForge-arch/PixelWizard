addde7c90275610e0f8040799d22c4db52b5d77f
李文雪 <liwenxue@xiaomi.com>
2025-11-03 22:00:59 +0800

    m

--- model_wan_trainer_upsample.py
+++ model_wan_trainer_upsample.py
@@ -154,16 +154,13 @@ class WanModel_Trainer:
             clean_latent = self.model.vae.encode_to_latent(frames).to(device=self.device, dtype=self.dtype)           # [1, 13, 48, 90, 160]
             clean_latent_lr = self.model.vae.encode_to_latent(frames_480p).to(device=self.device, dtype=self.dtype)   # [1, 13, 48, 30, 52]
             
-            print(f"frames.shape: {frames.shape}, clean_latent.shape: {clean_latent.shape}")
-            print(f"frames_480p.shape: {frames_480p.shape}, clean_latent_lr.shape: {clean_latent_lr.shape}")
-            exit()
             
             B, T, C, H, W   = clean_latent.shape
             _, _, _, h, w   = clean_latent_lr.shape
 
-            clean_latent_lr = clean_latent_lr.permute(0, 2, 1, 3, 4).reshape(B*T, C, h, w)  # [B*T, C, h, w]
+            clean_latent_lr = clean_latent_lr.reshape(B*T, C, h, w)  # [B*T, C, h, w]
             clean_latent_lr = F.interpolate(clean_latent_lr, size=(H, W), mode='bilinear', align_corners=False)  # 可加 antialias=True（若版本支持）
-            clean_latent_lr = clean_latent_lr.reshape(B, T, C, H, W).permute(0, 2, 1, 3, 4)  # 还原回 [B, C, T, H, W]
+            clean_latent_lr = clean_latent_lr.reshape(B, T, C, H, W)
                     
             # print(f"frames.shape: {frames.shape}, clean_latent.shape: {clean_latent.shape}")
             # print(f"frames_480p.shape: {frames_480p.shape}, clean_latent_lr.shape: {clean_latent_lr.shape}")
