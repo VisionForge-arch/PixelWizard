689215c4ebdd91e4f18d42ed18654da5e9728b98
李文雪 <liwenxue@xiaomi.com>
2025-11-03 17:17:04 +0800

    m

--- Wan2.2/wan/textimage2video_sr.py
+++ Wan2.2/wan/textimage2video_sr.py
@@ -373,7 +373,7 @@ class WanTI2V_SR:
                     generator=seed_g)
             ]
         
-        print('noise_shape:', noise.shape)    # [1, 48, 31, 90, 160]
+        print('noise_shape:', noise[0].shape)    # [1, 48, 31, 90, 160]
         print('cond_latent_shape:', cond_latent.shape)  # [1, 48, 31, 90, 160]
         
 
