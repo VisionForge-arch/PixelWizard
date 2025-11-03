86899b2aca9fdddcd0f04bdf06bd704b46c8186c
李文雪 <liwenxue@xiaomi.com>
2025-11-03 12:38:27 +0800

    m

--- Wan2.2/generate_multiple_sr.py
+++ Wan2.2/generate_multiple_sr.py
@@ -348,9 +348,11 @@ def encode_to_latent(model, pixel: torch.Tensor) -> torch.Tensor:
     print("in the fx, the pixel shape is:")
     print(pixel.shape)
     output = [
-        model.vae.encode(u.unsqueeze(0)).float().squeeze(0)
+        model.vae.encode([u.unsqueeze(0)])[0].float().squeeze(0)
         for u in pixel
     ]
+    
+    #output = model.vae.encode(u.unsqueeze(0)).float().squeeze(0)
     print("in the fx, the output shape is:")
     print(output.shape)
     output = torch.stack(output, dim=0)
