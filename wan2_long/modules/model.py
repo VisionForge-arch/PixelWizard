8b14753171b43a66537dc8c2af4e66a014fc2f49
李文雪 <liwenxue@xiaomi.com>
2025-11-04 19:28:05 +0800

    m

--- wan2_long/modules/model.py
+++ wan2_long/modules/model.py
@@ -583,9 +583,9 @@ class WanModel(ModelMixin, ConfigMixin):
             self.freqs = torch.cat([
                 rope_params(1024, d - 4 * (d // 6)),
                 rope_params(1024, 2 * (d // 6),
-                            scaling="yarn", factor=8.0, yarn_alpha=0.8, yarn_short_factor=1.0),
+                            scaling="yarn", factor=4.0, yarn_alpha=0.8, yarn_short_factor=1.0),
                 rope_params(1024, 2 * (d // 6),
-                            scaling="yarn", factor=8.0, yarn_alpha=0.8, yarn_short_factor=1.0)
+                            scaling="yarn", factor=4.0, yarn_alpha=0.8, yarn_short_factor=1.0)
             ], dim=1)
         else:
             self.freqs = torch.cat([
