9739a0f26f0db6f33d3fb061fa740a5a1d469db1
李文雪 <liwenxue@xiaomi.com>
2025-11-05 15:17:59 +0800

    m

--- Wan2.2/wan/modules/model.py
+++ Wan2.2/wan/modules/model.py
@@ -419,7 +419,8 @@ class WanModel(ModelMixin, ConfigMixin):
         if self.rope_scaling == "yarn":
             print("========= Using yarn rope scaling ==========")
             self.freqs = torch.cat([
-                rope_params(1024, d - 4 * (d // 6)),
+                rope_params(1024, d - 4 * (d // 6),
+                            scaling="yarn", factor=2.0, yarn_alpha=0.8, yarn_short_factor=1.0),
                 rope_params(1024, 2 * (d // 6),
                             scaling="yarn", factor=2.0, yarn_alpha=0.8, yarn_short_factor=1.0),
                 rope_params(1024, 2 * (d // 6),
