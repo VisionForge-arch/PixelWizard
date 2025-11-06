d4367e497d169a52bcfd5eb53d42192c4c829097
李文雪 <liwenxue@xiaomi.com>
2025-11-06 11:39:17 +0800

    m

--- Wan2.2/generate_multiple.py
+++ Wan2.2/generate_multiple.py
@@ -304,7 +304,7 @@ def _parse_args():
         "--wan_ckpt",
         type=str,
         #default=None,
-        default="/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/Ultra_Train_Weight/wan_2k_yarn/checkpoint_model_000900/model.pt",
+        default="/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/Ultra_Train_Weight/wan_2k_yarn/checkpoint_model_001000/model.pt",
         help="The path to the Wan checkpoint.")
     args = parser.parse_args()
     _validate_args(args)
