3411eb59deb8a9cd94f5ab162c0f2cd9c07e5a29
李文雪 <liwenxue@xiaomi.com>
2025-11-12 19:52:35 +0800

    m

--- Wan2.2/decode_list.py
+++ Wan2.2/decode_list.py
@@ -115,8 +115,8 @@ def generate_output_filename(prompt, resolution, timestamp):
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--input_dir", type=str, default="/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/outputs_ultra/2k_train_900iter_yarn/2k_yarn_zeroshot")
-    parser.add_argument("--output_dir", type=str, default="/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/outputs_ultra/2k_train_900iter_yarn/2k_yarn_zeroshot/decode_video")
+    parser.add_argument("--input_dir", type=str, default="/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/outputs_ultra/2k_train_900iter_yarn/2k_yarn_2200")
+    parser.add_argument("--output_dir", type=str, default="/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/outputs_ultra/2k_train_900iter_yarn/2k_yarn_2200/decode_video")
     parser.add_argument("--vae_path", type=str, default="/mnt/vision-gen-ks3/ModelZoo/Video_Generation/Wan2.2-TI2V-5B/Wan2.2_VAE.pth")
     parser.add_argument("--num_patches", type=int, default=2, help="分成几个patch进行decode，默认4")
     parser.add_argument("--patch_dim", type=str, default="w", choices=['h', 'w'], help="在哪个维度分割，h=高度，w=宽度")
