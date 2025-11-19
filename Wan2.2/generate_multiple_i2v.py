8cd094bf40f5c1863186479cf259e8a04a1e281d
李文雪 <liwenxue@xiaomi.com>
2025-11-19 21:48:04 +0800

    m

--- Wan2.2/generate_multiple_i2v.py
+++ Wan2.2/generate_multiple_i2v.py
@@ -162,7 +162,7 @@ def _parse_args():
     parser.add_argument(
         "--save_file",
         type=str,
-        default="/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/outputs_ultra/480p_base/480p_5s",
+        default="/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/outputs_ultra/i2v/720p",
         help="The file to save the generated video to.")
     parser.add_argument(
         "--prompt",
