eeabfa52e676fae1e3fc35e81a336922bd0d76b0
李文雪 <liwenxue@xiaomi.com>
2025-11-20 20:12:33 +0800

    m

--- Wan2.2/generate_multiple_i2v.py
+++ Wan2.2/generate_multiple_i2v.py
@@ -118,7 +118,7 @@ def _parse_args():
     parser.add_argument(
         "--size",
         type=str,
-        default="1920*1080",
+        default="2560*1440",
         choices=list(SIZE_CONFIGS.keys()),
         help="The area (width*height) of the generated video. For the I2V task, the aspect ratio of the output video will follow that of the input image."
     )
@@ -162,7 +162,7 @@ def _parse_args():
     parser.add_argument(
         "--save_file",
         type=str,
-        default="/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/outputs_ultra/i2v/1080p",
+        default="/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/outputs_ultra/i2v/1440p_train",
         help="The file to save the generated video to.")
     parser.add_argument(
         "--prompt",
@@ -306,7 +306,7 @@ def _parse_args():
         "--wan_ckpt",
         type=str,
         #default=None,
-        #default="/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/Ultra_Train_Weight/wan_i2v/checkpoint_model_000500/model.pt",
+        default="/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/Ultra_Train_Weight/wan_i2v/checkpoint_model_000100/model.pt",
         help="The path to the Wan checkpoint.")
     args = parser.parse_args()
     _validate_args(args)
@@ -366,7 +366,7 @@ def generate(args):
     
     # 定义要使用的分辨率
     #resolutions = ['1920*1056', '2560*1440', '3840*2144'，'1280*704']
-    resolutions = ['1920*1080']
+    resolutions = ['2560*1440']
     
     # 创建保存文件夹
     output_dir = args.save_file 
