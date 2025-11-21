2d0261eed70b6fb9b21fd4bd1687d4aa9663a3e3
李文雪 <liwenxue@xiaomi.com>
2025-11-21 23:04:58 +0800

    m

--- Wan2.2/wan/textimage2video_sr.py
+++ Wan2.2/wan/textimage2video_sr.py
@@ -514,8 +514,8 @@ class WanTI2V_SR:
             print(f'   CFG scale: {guide_scale}, Shift: {shift}')
             
             for i, t in enumerate(tqdm(timesteps)):
-                fused_latent = torch.cat([latents[0], cond_latent], dim=0).unsqueeze(0)  # [1, C_hr+C_lr, T, H, W]
-                fused_latent = self.sr_proj(fused_latent).squeeze(0)
+                fused_latent = torch.cat([latents[0], cond_latent], dim=0)#.unsqueeze(0)  # [1, C_hr+C_lr, T, H, W]
+                #fused_latent = self.sr_proj(fused_latent).squeeze(0)
                 latent_model_input = [fused_latent]
 
                 timestep = [t]
