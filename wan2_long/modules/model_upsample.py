8a2576f19d6d42c9503cc398e2413de029517067
李文雪 <liwenxue@xiaomi.com>
2025-11-22 23:31:51 +0800

    m

--- wan2_long/modules/model_upsample.py
+++ wan2_long/modules/model_upsample.py
@@ -778,8 +778,7 @@ class WanModel_Upsample(ModelMixin, ConfigMixin):
         x = [u.flatten(2).transpose(1, 2) for u in x]         # list 1个 [1, t*h*w, 3072]
         seq_lens = torch.tensor([u.size(1) for u in x], dtype=torch.long)  # t*h*w
         
-        print(seq_lens.max())
-        print('='+seq_len)
+        print(seq_len)
         assert seq_lens.max() <= seq_len
         x = torch.cat([
             torch.cat([u, u.new_zeros(1, seq_len - u.size(1), u.size(2))],
