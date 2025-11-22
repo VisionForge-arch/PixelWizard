895543fe32d44bfe3b1aaeeb942a8a4b4c3eecc8
李文雪 <liwenxue@xiaomi.com>
2025-11-22 22:45:25 +0800

    m

--- model_wan_trainer_upsample.py
+++ model_wan_trainer_upsample.py
@@ -160,7 +160,7 @@ class WanModel_Trainer:
 
             clean_latent_lr = clean_latent_lr.reshape(B*T, C, h, w)  # [B*T, C, h, w]
             clean_latent_lr = F.interpolate(clean_latent_lr, size=(H, W), mode='bilinear', align_corners=False)  # 可加 antialias=True（若版本支持）
-            clean_latent_lr = clean_latent_lr.reshape(B, T, C, H, W)
+            clean_latent_lr = clean_latent_lr.reshape(B, T, C, H, W).permute(0, 2, 1, 3, 4).contiguous()
                     
             # print(f"frames.shape: {frames.shape}, clean_latent.shape: {clean_latent.shape}")
             # print(f"frames_480p.shape: {frames_480p.shape}, clean_latent_lr.shape: {clean_latent_lr.shape}")
