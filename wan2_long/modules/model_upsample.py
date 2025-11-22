599aa76ff8d3b6acdd26952a87a5bf4a58e45b65
李文雪 <liwenxue@xiaomi.com>
2025-11-22 21:12:42 +0800

    m

--- wan2_long/modules/model_upsample.py
+++ wan2_long/modules/model_upsample.py
@@ -451,7 +451,7 @@ class WanSpatialControlAdapter(nn.Module):
         """
         # A. 提取特征
         x = self.backbone(lr_latents)  # [B, C, T, H, ]
-        
+        print(x.shape)
         x = x.flatten(2).transpose(1, 2) # [B, SeqLen, Dim]
         
         x = self.feature_norm(x)
