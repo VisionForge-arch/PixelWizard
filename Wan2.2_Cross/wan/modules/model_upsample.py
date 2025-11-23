d3b13612ac0c5a2cccfbda6e73e521b71ce6e2c0
李文雪 <liwenxue@xiaomi.com>
2025-11-23 19:03:48 +0800

    m

--- Wan2.2_Cross/wan/modules/model_upsample.py
+++ Wan2.2_Cross/wan/modules/model_upsample.py
@@ -367,7 +367,7 @@ class WanSpatialControlAdapter(nn.Module):
         t_sinusoidal_emb: [B, freq_dim] <- 这是原始的正弦位置编码
         """
         # A. 提取特征
-        #print(lr_latents.shape)
+        print(lr_latents.shape)
         
         x = self.backbone(lr_latents)  # [B, C, T, H, ]
         x = x.flatten(2).transpose(1, 2) # [B, SeqLen, Dim]
