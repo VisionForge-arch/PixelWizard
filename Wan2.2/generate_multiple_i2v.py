26d3a6eed7777a1bd807e0b9d2fb966093fe5abc
李文雪 <liwenxue@xiaomi.com>
2025-11-25 21:51:42 +0800

    m

--- Wan2.2/generate_multiple_i2v.py
+++ Wan2.2/generate_multiple_i2v.py
@@ -118,7 +118,7 @@ def _parse_args():
     parser.add_argument(
         "--size",
         type=str,
-        default="1920*1056",
+        default="1280*704",
         choices=list(SIZE_CONFIGS.keys()),
         help="The area (width*height) of the generated video. For the I2V task, the aspect ratio of the output video will follow that of the input image."
     )
@@ -162,7 +162,7 @@ def _parse_args():
     parser.add_argument(
         "--save_file",
         type=str,
-        default="/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/outputs_ultra/i2v/1080p",
+        default="/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/outputs_ultra/i2v/720p",
         help="The file to save the generated video to.")
     parser.add_argument(
         "--prompt",
