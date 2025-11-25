9e00075035211d676a37e55c20cdefefdfe89b46
李文雪 <liwenxue@xiaomi.com>
2025-11-25 09:46:49 +0800

    m

--- Wan2.2_Cross/generate_multiple_upsample.py
+++ Wan2.2_Cross/generate_multiple_upsample.py
@@ -164,7 +164,7 @@ def _parse_args():
     parser.add_argument(
         "--save_file",
         type=str,
-        default="/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/outputs_ultra/480p_base/480p_upsample_1",
+        default="/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/outputs_ultra/480p_base/480p_upsample_2",
         help="The file to save the generated video to.")
     parser.add_argument(
         "--prompt",
@@ -308,7 +308,7 @@ def _parse_args():
         "--wan_ckpt",
         type=str,
         #default=None,
-        default="/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/Ultra_Train_Weight/wan_latent_up/checkpoint_model_001500/model.pt",
+        default="/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/Ultra_Train_Weight/wan_latent_up/checkpoint_model_001600/model.pt",
         help="The path to the Wan checkpoint.")
     args = parser.parse_args()
     _validate_args(args)
