90a0b2ab1ef92a9d3794459b9a8f222353d9a0cc
李文雪 <liwenxue@xiaomi.com>
2025-11-26 20:59:00 +0800

    m

--- wan2_long/modules/model_upsample_causal.py
+++ wan2_long/modules/model_upsample_causal.py
@@ -7,8 +7,8 @@ from diffusers.configuration_utils import ConfigMixin, register_to_config
 from diffusers.models.modeling_utils import ModelMixin
 from torch.nn.attention.flex_attention import BlockMask, create_block_mask, flex_attention
 
-from .attention import attention
-from .model import (
+from attention import attention
+from model import (
     MLPProj,
     WAN_CROSSATTENTION_CLASSES,
     WanLayerNorm,
@@ -18,7 +18,7 @@ from .model import (
     rope_params,
     sinusoidal_embedding_1d,
 )
-
+from model_upsample import register_spatial_control
 __all__ = ["WanModel_Upsample"]
 
 
@@ -653,7 +653,7 @@ class WanModel_Upsample(ModelMixin, ConfigMixin):
 
 if __name__ == "__main__":
     # Debug helper, keep for quick manual testing.
-    from .model_upsample import register_spatial_control
+    
 
     model = WanModel_Upsample.from_pretrained(f"/mnt/vision-gen-ks3/ModelZoo/Video_Generation/Wan2.2-TI2V-5B")
     model, lr_layers = register_spatial_control(model)
