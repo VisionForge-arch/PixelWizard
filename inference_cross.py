42fc0cbc8073bb8f87b91d0a200fd7c68ccff044
李文雪 <liwenxue@xiaomi.com>
2025-12-01 20:43:25 +0800

    m

--- inference_cross.py
+++ inference_cross.py
@@ -28,7 +28,7 @@ parser = argparse.ArgumentParser()
 parser.add_argument("--config_path", type=str, default="./configs/self_forcing_dmd.yaml", help="Path to the config file")
 parser.add_argument("--checkpoint_path", type=str, default="/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/Ultra_Train_Weight/wan_latent_up_2/checkpoint_model_009800/model.pt", help="Path to the checkpoint folder")
 parser.add_argument("--prompt_file", type=str, default="/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/prompt_to_file.json", help="JSON file with prompts/files for upsample inference")
-parser.add_argument("--output_folder", type=str, default="/hpc2hdd/home/htian395/Wenxue/Self-Forcing-Long/outputs/", help="Output folder")
+parser.add_argument("--output_folder", type=str, default="/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/outputs_ultra/ar_upsample", help="Output folder")
 parser.add_argument("--num_output_frames", type=int, default=31, help="Number of overlap frames between sliding windows")
 parser.add_argument("--i2v", action="store_true", help="Whether to perform I2V (or T2V by default)")
 parser.add_argument("--use_ema", action="store_true", default=True, help="Whether to use EMA parameters")
@@ -228,7 +228,7 @@ for i, batch_data in tqdm(enumerate(dataloader), disable=(local_rank != 0)):
     file_name = f"latent_{i}_{formatted_prompt}_{formatted_time}.pt"
     output_path = os.path.join(args.output_folder, file_name)
 
-    logging.info(f"Saving latent to {output_path}")
+    print(f"Saving latent to {output_path}")
     torch.save(
         {
             "latent": latents.cpu(),
@@ -240,7 +240,7 @@ for i, batch_data in tqdm(enumerate(dataloader), disable=(local_rank != 0)):
         },
         output_path,
     )
-    logging.info("Latent saved successfully.")
+    print("Latent saved successfully.")
 
     if dist.is_initialized():
         dist.barrier()
