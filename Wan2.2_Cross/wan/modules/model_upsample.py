c52ddfeb2657ffbfa878f92b0ccd4928ea5700cd
李文雪 <liwenxue@xiaomi.com>
2025-12-04 15:01:33 +0800

    m

--- Wan2.2_Cross/wan/modules/model_upsample.py
+++ Wan2.2_Cross/wan/modules/model_upsample.py
@@ -429,7 +429,7 @@ def register_spatial_control(model):
             # ⭐⭐ 关键 1：只在前 num_control_blocks 层注入，后面的层不做任何事
             # if block_idx >= 10:
             #     return args
-            if  block_idx >= 10 and block_idx % 6 != 0:
+            if  block_idx % 6 != 0:
                 return args
              # ==========================================================
 
