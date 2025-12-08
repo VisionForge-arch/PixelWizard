ad1dd459cfd1a7d32f7a5f59ac6d1c0ee1add3ce
李文雪 <liwenxue@xiaomi.com>
2025-12-08 21:48:33 +0800

    m

--- Wan2.2_Cross/generate_multiple_upsample.py
+++ Wan2.2_Cross/generate_multiple_upsample.py
@@ -119,7 +119,7 @@ def _parse_args():
     parser.add_argument(
         "--size",
         type=str,
-        default="2560*1440",
+        default="2560*1440",  # 3840*2144
         choices=list(SIZE_CONFIGS.keys()),
         help="The area (width*height) of the generated video. For the I2V task, the aspect ratio of the output video will follow that of the input image."
     )
@@ -507,6 +507,9 @@ def generate(args):
                 generate_resolution_H, generate_resolution_W = generate_resolution
                 H = int(generate_resolution_H // 16)
                 W = int(generate_resolution_W // 16)
+                print(H)
+                print(W)
+                exit()
                 print("cond_latent_lr.shape:", cond_latent_lr.shape)
                 cond_latent_lr = cond_latent_lr.permute(0, 2, 1, 3, 4)  
                 cond_latent_lr = cond_latent_lr.reshape(B*T, C, h, w)  # [B*C, T, h, w]
