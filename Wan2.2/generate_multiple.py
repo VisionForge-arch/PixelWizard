2b701da8e8c7768ad20ae46c484cfceb5e6009bc
李文雪 <liwenxue@xiaomi.com>
2025-12-10 15:33:32 +0800

    m

--- Wan2.2/generate_multiple.py
+++ Wan2.2/generate_multiple.py
@@ -116,7 +116,7 @@ def _parse_args():
     parser.add_argument(
         "--size",
         type=str,
-        default="832*480",
+        default="1280*704",
         choices=list(SIZE_CONFIGS.keys()),
         help="The area (width*height) of the generated video. For the I2V task, the aspect ratio of the output video will follow that of the input image."
     )
@@ -160,7 +160,7 @@ def _parse_args():
     parser.add_argument(
         "--save_file",
         type=str,
-        default="/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/outputs_ultra/tempt_test",
+        default="/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/outputs_ultra/480p_base/720p_5s",
         help="The file to save the generated video to.")
     parser.add_argument(
         "--prompt",
@@ -298,7 +298,7 @@ def _parse_args():
     parser.add_argument(
         "--prompt_file",
         type=str,
-        default="/root/ultrawan/Wan2.2/prompt.txt",
+        default="/root/ultrawan/Wan2.2/prompt3.txt",
         help="The file to read the prompts from.")
     parser.add_argument(
         "--wan_ckpt",
