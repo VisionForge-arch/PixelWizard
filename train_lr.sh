9e1bde686504e1ab5a437cf3c13e76149c69ccfa
李文雪 <liwenxue@xiaomi.com>
2025-12-11 22:19:50 +0800

    m

--- train_lr.sh
+++ train_lr.sh
@@ -1,6 +1,5 @@
-export CUDA_VISIBLE_DEVICES=1,2,3,4,5,6,7
 export WANDB_MODE=offline
 
-torchrun --standalone --nproc_per_node=7 \
+torchrun --standalone --nproc_per_node=4 \
   train_lr.py \
   --use_ema 
