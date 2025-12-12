df78b05eb86bd118d8fc2ba51947c71923c49d7e
李文雪 <liwenxue@xiaomi.com>
2025-12-12 15:44:10 +0800

    m

--- Wan2.2/prompt_match.py
+++ Wan2.2/prompt_match.py
@@ -5,10 +5,10 @@ import json
 
 # === 配置路径 ===
 prompt_txt = "/root/ultrawan/Wan2.2/prompt.txt"                   # 每行一个 prompt
-video_dir  = "/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/outputs_ultra/480p_base/720p_5s/decode_video0"        # 存放 .mp4 或 .pt 文件的目录
+video_dir  = "/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/outputs_ultra/480p_base/360p_5s/decoded_video_3900"        # 存放 .mp4 或 .pt 文件的目录
 
 
-pat = re.compile(r"^ti2v-5B_1280\*704_1_(?:\+?prompt_)?(?P<frag>.+?)_\d{8}_\d{6}\.(?:pt|mp4)$")
+pat = re.compile(r"^ti2v-5B_640\*352_1_(?:\+?prompt_)?(?P<frag>.+?)_\d{8}_\d{6}\.(?:pt|mp4)$")
 
 def normalize(p: str) -> str:
     # 和你保存时完全一致：只做空格与斜杠替换，然后截断 50
@@ -47,7 +47,7 @@ for p in prompts:
             "file": os.path.join(video_dir, exact)
         })
 
-out_json = "/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/prompt_to_file_720p.json"
+out_json = "/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/prompt_to_file_360p.json"
 with open(out_json, "w", encoding="utf-8") as f:
     json.dump(mapping, f, indent=2, ensure_ascii=False)
 
