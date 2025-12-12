82bcd3d2b3dcbf7e13f84e85123727cde918f8f7
李文雪 <liwenxue@xiaomi.com>
2025-12-12 15:52:26 +0800

    m

--- Wan2.2_Cross/infer_multi.sh
+++ Wan2.2_Cross/infer_multi.sh
@@ -1,27 +1,27 @@
-torchrun --standalone --nproc_per_node=4 \
-    generate_multiple_upsample.py \
-    --size=2560*1440 \
-    --sample_steps=15 \
-    --prompt_file=/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/prompt_to_file.json \
-    --wan_ckpt=/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/Ultra_Train_Weight/wan_latent_up_2_time_modulation_4k/checkpoint_model_000500/model.pt \
-    --frame_num=121 \
-    --use_ema \
-    --sample_shift=5.5 \
-    --dit_fsdp \
-    --t5_fsdp \
-    --ulysses_size 4 \
-
-
-
 # torchrun --standalone --nproc_per_node=4 \
 #     generate_multiple_upsample.py \
-#     --size=3840*2144 \
+#     --size=2560*1440 \
 #     --sample_steps=15 \
-#     --prompt_file=/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/prompt_to_file_720p.json \
-#     --wan_ckpt=/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/Ultra_Train_Weight/wan_latent_up_2_time_modulation_4k/checkpoint_model_000500/model.pt \
+#     --prompt_file=/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/prompt_to_file_360p.json \
+#     --wan_ckpt=/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/Ultra_Train_Weight/wan_latent_up_2_time_modulation2/checkpoint_model_000800/model.pt \
 #     --frame_num=121 \
 #     --use_ema \
 #     --sample_shift=5.5 \
 #     --dit_fsdp \
 #     --t5_fsdp \
 #     --ulysses_size 4 \
+
+
+
+torchrun --standalone --nproc_per_node=8 \
+    generate_multiple_upsample.py \
+    --size=3840*2144 \
+    --sample_steps=15 \
+    --prompt_file=/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/prompt_to_file_360p.json \
+    --wan_ckpt=/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/Ultra_Train_Weight/wan_latent_up_2_time_modulation_4k/checkpoint_model_000500/model.pt \
+    --frame_num=121 \
+    --use_ema \
+    --sample_shift=5.5 \
+    --dit_fsdp \
+    --t5_fsdp \
+    --ulysses_size 8 \
