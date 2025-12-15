009b224fd0990ecc2f2cb27bca9f8f44cf20cbdf
李文雪 <liwenxue@xiaomi.com>
2025-12-15 13:35:54 +0800

    m

--- Wan2.2/infer.sh
+++ Wan2.2/infer.sh
@@ -1,5 +1,7 @@
-torchrun --nproc_per_node=4 \
-    generate.py \
+torchrun --nproc_per_node=8 \
+    generate_multiple.py \
+    --wan_ckpt /mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/Ultra_Train_Weight/wan_360p_resume/checkpoint_model_006000/model.pt \
+    --use_ema \
     --dit_fsdp \
     --t5_fsdp \
-    --ulysses_size 4 \
+    --ulysses_size 8 \
