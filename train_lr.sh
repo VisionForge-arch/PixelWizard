ab1ee2ff9ab547aed137835fcb2ab807ecb6fbdc
李文雪 <liwenxue@xiaomi.com>
2025-12-15 15:56:50 +0800

    m

--- train_lr.sh
+++ train_lr.sh
@@ -1,5 +1,16 @@
 export WANDB_MODE=offline
+# 在所有节点都设置这些变量
+export MASTER_ADDR=10.44.140.186  # 主节点 IP
+export MASTER_PORT=29500            # 通讯端口
+export NNODES=2                          # 总节点数
+export NODE_RANK=0                      # 当前节点序号，主节点0，从节点1,2,...
 
-torchrun --standalone --nproc_per_node=8 \
+
+torchrun \
+  --nnodes=${NNODES} \
+  --node_rank=${NODE_RANK} \
+  --nproc_per_node=8 \
+  --master_addr=${MASTER_ADDR} \
+  --master_port=${MASTER_PORT} \
   train_lr.py \
-  --use_ema   \
+  --use_ema   
