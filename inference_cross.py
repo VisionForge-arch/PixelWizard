4d6059d393ebf00a05eec8f20514d6668611499d
李文雪 <liwenxue@xiaomi.com>
2025-12-18 14:01:22 +0800

    m

--- inference_cross.py
+++ inference_cross.py
@@ -64,8 +64,8 @@ config.sr_mode = True
 config.causal = True
 config.kv_cache_height = int(config.height // 32)
 config.kv_cache_width = int(config.width // 32)
-# config.kv_cache_time = args.num_output_frames + 4
-config.kv_cache_time = 12
+config.kv_cache_time = args.num_output_frames + 1
+#config.kv_cache_time = 12
 # recompute seq_len based on height/width/num_frames from config
 # time_part = int((config.num_frames - 1) // 4) + 1
 time_part = args.num_output_frames
