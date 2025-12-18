754927a6b6c27b96ba92631d0549291711ff2347
李文雪 <liwenxue@xiaomi.com>
2025-12-18 10:50:55 +0800

    m

--- train_sr_causal.sh
+++ train_sr_causal.sh
@@ -1,8 +1,8 @@
-export CUDA_VISIBLE_DEVICES=0,1,2,3
+export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 export WANDB_MODE=offline
 export CUDA_LAUNCH_BLOCKING=1
 
-torchrun --standalone --nproc_per_node=4 \
+torchrun --standalone --nproc_per_node=8 \
   train_sr_causal.py \
   --load_generator_ckpt \
   --trainable_backbone \
