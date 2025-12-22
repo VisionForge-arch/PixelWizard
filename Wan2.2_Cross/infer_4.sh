7200a36e2b8a8936c05009b305ce2393cfb7a79e
李文雪 <liwenxue@xiaomi.com>
2025-12-22 09:45:32 +0800

    m

--- Wan2.2_Cross/infer_4.sh
+++ Wan2.2_Cross/infer_4.sh
@@ -1,4 +1,4 @@
-torchrun --standalone --nproc_per_node=4 \
+torchrun --standalone --nproc_per_node=8 \
     generate_multiple_upsample_shortcut.py \
     --size=2560*1440 \
     --sample_steps=3 \
@@ -8,4 +8,4 @@ torchrun --standalone --nproc_per_node=4 \
     --sample_shift=5.5 \
     --dit_fsdp \
     --t5_fsdp \
-    --ulysses_size 4 \
+    --ulysses_size 8 \
