7e085127fd107c5d5f3a85761edde66b1448e446
李文雪 <liwenxue@xiaomi.com>
2025-12-25 10:22:47 +0800

    m

--- Wan2.2/infer_multi.sh
+++ Wan2.2/infer_multi.sh
@@ -1,7 +1,6 @@
-export CUDA_VISIBLE_DEVICES=1,2,3,4,5,6,7
 
-torchrun --nproc_per_node=7 \
+torchrun --nproc_per_node=8 \
     generate_multiple.py \
     --dit_fsdp \
     --t5_fsdp \
-    --ulysses_size 7 \
+    --ulysses_size 8 \
