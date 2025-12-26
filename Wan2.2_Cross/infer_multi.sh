8a72234afd5be9a514948ee6a23b1ea04b634085
李文雪 <liwenxue@xiaomi.com>
2025-12-26 19:42:48 +0800

    add

--- Wan2.2_Cross/infer_multi.sh
+++ Wan2.2_Cross/infer_multi.sh
@@ -1,10 +1,10 @@
 torchrun --standalone --nproc_per_node=4 \
     generate_multiple_upsample.py \
     --size=2560*1440 \
-    --sample_steps=4 \
+    --sample_steps=3 \
     --prompt_file=/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/prompts/prompt_to_file_240p.json \
     --wan_ckpt=/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/Ultra_Train_Weight/wan_latent_up_2_time_modulation2/checkpoint_model_000800/model.pt \
-    --save_file=/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/outputs_ultra/480p_base/240p_upsample_2k \
+    --save_file=/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/outputs_ultra/480p_base/240p_upsample_2k/pt \
     --frame_num=121 \
     --use_ema \
     --sample_shift=5.5 \
