d4115b96b9bcc1d748ec27e5d37267815a0cb663
李文雪 <liwenxue@xiaomi.com>
2025-12-26 15:32:44 +0800

    add

--- Wan2.2_Cross/infer_4.sh
+++ Wan2.2_Cross/infer_4.sh
@@ -1,12 +1,27 @@
+# torchrun --standalone --nproc_per_node=8 \
+#     generate_multiple_upsample_shortcut.py \
+#     --size=2560*1440 \
+#     --sample_steps=5 \
+#     --prompt_file=/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/prompts/prompt_to_file_240p.json \
+#     --wan_ckpt=/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/Ultra_Train_Weight/2k_shortcut2/checkpoint_model_000400/model.pt \
+#     --save_file=/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/outputs_ultra/480p_base/240p_upsample_2k_shortcut2/pt \
+#     --frame_num=121 \
+#     --sample_shift=5.5 \
+#     --dit_fsdp \
+#     --t5_fsdp \
+#     --ulysses_size 8 \
+
+
 torchrun --standalone --nproc_per_node=8 \
     generate_multiple_upsample_shortcut.py \
     --size=2560*1440 \
-    --sample_steps=5 \
+    --sample_steps=3 \
     --prompt_file=/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/prompts/prompt_to_file_240p.json \
-    --wan_ckpt=/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/Ultra_Train_Weight/2k_shortcut2/checkpoint_model_000400/model.pt \
-    --save_file=/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/outputs_ultra/480p_base/240p_upsample_2k_shortcut2/pt \
+    --wan_ckpt=/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/Ultra_Train_Weight/2k_shortcut/checkpoint_model_000800/model.pt \
+    --save_file=/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/outputs_ultra/480p_base/240p_upsample_2k_shortcut/pt3 \
     --frame_num=121 \
     --sample_shift=5.5 \
+    --use_ema \
     --dit_fsdp \
     --t5_fsdp \
     --ulysses_size 8 \
