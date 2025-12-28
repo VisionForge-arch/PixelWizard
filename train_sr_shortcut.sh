cfb99ff51664278f886a24c8fa04c1be60cece35
李文雪 <liwenxue@xiaomi.com>
2025-12-28 19:10:08 +0800

    add

--- train_sr_shortcut.sh
+++ train_sr_shortcut.sh
@@ -3,6 +3,12 @@ export WANDB_MODE=offline
 
 torchrun --standalone --nproc_per_node=8 \
   train_sr.py \
+  --max_pixels=3840*2144 \
+  --height=2144 \
+  --width=3840 \
+  --config_path=/root/ultrawan/configs/self_forcing_dmd_4k.yaml \
+  --logdir=/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/Ultra_Train_Weight/4k_shortcut \
+  --generator_ckpt=/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/Ultra_Train_Weight/wan_latent_up_2_time_modulation2/checkpoint_model_000800/model.pt \
   --trainable_backbone \
   --shortcut \
   --load_generator_ckpt \
