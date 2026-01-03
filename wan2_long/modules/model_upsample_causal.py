700ef6fca262c97dd97f89fc32665cf51c87f027
李文雪 <liwenxue@xiaomi.com>
2026-01-03 14:16:45 +0800

    add

--- wan2_long/modules/model_upsample_causal.py
+++ wan2_long/modules/model_upsample_causal.py
@@ -105,6 +105,9 @@ class CausalWanSelfAttention(nn.Module):
             return q, k, v
 
         q, k, v = qkv_fn(x)
+        
+        
+        kv_cache = None
 
         if kv_cache is None:
             roped_query = rope_apply(q, grid_sizes, freqs).type_as(v)
