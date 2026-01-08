3a718cad38f2a1836a4a985986d5e51847645927
李文雪 <liwenxue@xiaomi.com>
2026-01-08 19:53:40 +0800

    m

--- Wan2.2_Cross/infer_2k.sh
+++ Wan2.2_Cross/infer_2k.sh
@@ -3,9 +3,9 @@ torchrun --standalone --nproc_per_node=6 \
     generate_multiple_upsample_shortcut.py \
     --size=2560*1440 \
     --sample_steps=4 \
-    --prompt_file=/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/prompts/woman_240p_pt.json \
-    --wan_ckpt=/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/Ultra_Train_Weight/2k_shortcut_new/checkpoint_model_004050/model.pt \
-    --save_file=/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/outputs_ultra/480p_base/240p_upsample_2k_shortcut2/pt_woman \
+    --prompt_file=/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/prompts/woman_240p_pt_20.json \
+    --wan_ckpt=/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/Ultra_Train_Weight/2k_shortcut_new/checkpoint_model_004350/model.pt \
+    --save_file=/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/outputs_ultra/480p_base/240p_upsample_2k_shortcut2/pt_woman_20 \
     --frame_num=121 \
     --sample_shift=5.5 \
     --dit_fsdp \
