95ccf0ff95dbc6158bcc63cdb03708572598e2b3
李文雪 <liwenxue@xiaomi.com>
2026-01-08 22:51:21 +0800

    m

--- train_sr_shortcut.sh
+++ train_sr_shortcut.sh
@@ -1,26 +1,28 @@
 export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 export WANDB_MODE=offline
 
+# torchrun --standalone --nproc_per_node=8 \
+#   train_sr.py \
+#   --height=2144 \
+#   --width=3840 \
+#   --num_frames=69 \
+#   --config_path=/root/ultrawan/configs/self_forcing_dmd_4k.yaml \
+#   --logdir=/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/Ultra_Train_Weight/4k_shortcut_new \
+#   --generator_ckpt=/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/Ultra_Train_Weight/2k_shortcut_new/checkpoint_model_001900/model.pt \
+#   --trainable_backbone \
+#   --shortcut \
+#   --load_generator_ckpt \
+
+
 torchrun --standalone --nproc_per_node=8 \
   train_sr.py \
-  --height=2144 \
-  --width=3840 \
-  --num_frames=69 \
-  --config_path=/root/ultrawan/configs/self_forcing_dmd_4k.yaml \
-  --logdir=/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/Ultra_Train_Weight/4k_shortcut_new \
-  --generator_ckpt=/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/Ultra_Train_Weight/2k_shortcut_new/checkpoint_model_001900/model.pt \
+  --height=1440 \
+  --width=2560 \
+  --num_frames=121 \
+  --config_path=/root/ultrawan/configs/self_forcing_dmd.yaml \
+  --logdir=/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/Ultra_Train_Weight/2k_shortcut_new2 \
+  --generator_ckpt=/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/Ultra_Train_Weight/2k_shortcut_new/checkpoint_model_002500/model.pt \
   --trainable_backbone \
   --shortcut \
   --load_generator_ckpt \
-
-
-# torchrun --standalone --nproc_per_node=8 \
-#   train_sr.py \
-#   --height=1440 \
-#   --width=2560 \
-#   --num_frames=69 \
-#   --config_path=/root/ultrawan/configs/self_forcing_dmd.yaml \
-#   --logdir=/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/Ultra_Train_Weight/2k_shortcut_new \
-#   --generator_ckpt=/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/Ultra_Train_Weight/wan_latent_up_2_time_modulation2/checkpoint_model_000800/model.pt \
-#   --trainable_backbone \
-#   --shortcut \
+  --use_ema \
