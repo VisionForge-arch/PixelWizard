b04c391c4092e9e7bb5cb90d3492b54bd0e9472d
李文雪 <liwenxue@xiaomi.com>
2026-01-09 13:44:01 +0800

    m

--- Wan2.2_Cross/infer_2k.sh
+++ Wan2.2_Cross/infer_2k.sh
@@ -1,11 +1,26 @@
 export CUDA_VISIBLE_DEVICES=2,1,4,5,6,7
+# torchrun --standalone --nproc_per_node=6 \
+#     generate_multiple_upsample_shortcut.py \
+#     --use_ema \
+#     --size=2560*1440 \
+#     --sample_steps=4 \
+#     --prompt_file=/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/prompts/woman_240p_pt_20.json \
+#     --wan_ckpt=/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/Ultra_Train_Weight/2k_shortcut_new/checkpoint_model_002100/model.pt \
+#     --save_file=/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/outputs_ultra/480p_base/240p_upsample_2k_shortcut2/pt_woman_20 \
+#     --frame_num=121 \
+#     --sample_shift=5.5 \
+#     --dit_fsdp \
+#     --t5_fsdp \
+#     --ulysses_size 6 \
+
+
 torchrun --standalone --nproc_per_node=6 \
     generate_multiple_upsample_shortcut.py \
     --use_ema \
     --size=2560*1440 \
     --sample_steps=4 \
     --prompt_file=/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/prompts/woman_240p_pt_20.json \
-    --wan_ckpt=/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/Ultra_Train_Weight/2k_shortcut_new2/checkpoint_model_000200/model.pt \
+    --wan_ckpt=/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/Ultra_Train_Weight/2k_shortcut_new/checkpoint_model_002100/model.pt \
     --save_file=/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/outputs_ultra/480p_base/240p_upsample_2k_shortcut2/pt_woman_20 \
     --frame_num=121 \
     --sample_shift=5.5 \
--- Wan2.2_Cross/prompt_match.py
+++ Wan2.2_Cross/prompt_match.py
@@ -4,8 +4,8 @@ import difflib
 import json
 
 # === 配置路径 ===
-prompt_txt = "/root/ultrawan/Wan2.2/prompt3.txt"                   # 每行一个 prompt
-video_dir  = "/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/outputs_ultra/480p_base/240p_woman/pt_20"        # 存放 .mp4 或 .pt 文件的目录
+prompt_txt = "/root/ultrawan/Wan2.2/prompt_env.txt"                   # 每行一个 prompt
+video_dir  = "/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/outputs_ultra/480p_base/240p_env/pt_20"        # 存放 .mp4 或 .pt 文件的目录
 
 
 pat = re.compile(r"^ti2v-5B_448\*256_8_(?:\+?prompt_)?(?P<frag>.+?)_\d{8}_\d{6}\.(?:pt|mp4)$")
@@ -47,7 +47,7 @@ for p in prompts:
             "file": os.path.join(video_dir, exact)
         })
 
-out_json = "/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/prompts/woman_240p_pt_20.json"
+out_json = "/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/prompts/env_240p_pt_20.json"
 with open(out_json, "w", encoding="utf-8") as f:
     json.dump(mapping, f, indent=2, ensure_ascii=False)
 
