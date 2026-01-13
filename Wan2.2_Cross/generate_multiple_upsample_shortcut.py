4c6520472bb3f1060c33e832e7a048d9cb104ee4
李文雪 <liwenxue@xiaomi.com>
2026-01-13 14:56:05 +0800

    m

--- Wan2.2_Cross/generate_multiple_upsample_shortcut.py
+++ Wan2.2_Cross/generate_multiple_upsample_shortcut.py
@@ -562,8 +562,7 @@ def generate(args):
 
             
             if rank == 0:
-                formatted_time = datetime.now().strftime("%Y%m%d_%H%M%S")
-                file_name = f"{str(prompt_idx)}_{formatted_time}.pt"
+                file_name = f"{str(prompt_idx)}.pt"
                 #formatted_time = datetime.now().strftime("%Y%m%d_%H%M%S")
                 #formatted_prompt = current_prompt.replace(" ", "_").replace("/", "_")[:50]
                 #file_name = f"{args.task}_{resolution.replace('*','x') if sys.platform=='win32' else resolution}_{args.ulysses_size}_{formatted_prompt}_{formatted_time}.pt"
