a2b1614dbb2ec99f7bc3792d22f1e4ee38fd1fef
李文雪 <liwenxue@xiaomi.com>
2026-01-13 20:39:27 +0800

    m

--- Wan2.2/infer_multi.sh
+++ Wan2.2/infer_multi.sh
@@ -26,11 +26,11 @@ export CUDA_VISIBLE_DEVICES=2,3,4,5,6,7
 torchrun --standalone --nproc_per_node=6 \
     generate_multiple.py \
     --sample_steps=50 \
-    --base_seed=0 \
+    --base_seed=1 \
     --use_ema \
     --dit_fsdp \
     --prompt_file=/root/ultrawan/Wan2.2/prompt2.txt \
-    --save_file=/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/outputs_ultra/480p_base/240p_woman_tx/pt \
+    --save_file=/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/outputs_ultra/480p_base/240p_woman_tx/pt_1 \
     --wan_ckpt=/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/Ultra_Train_Weight/wan_240p/checkpoint_model_000100/model.pt \
     --size=448*256 \
     --t5_fsdp \
