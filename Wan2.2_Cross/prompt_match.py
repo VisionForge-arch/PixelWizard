6ebd53375e02a382adc4cd09b48678cbb47cdeb4
李文雪 <liwenxue@xiaomi.com>
2026-01-14 18:43:19 +0800

    m

--- Wan2.2_Cross/prompt_match.py
+++ Wan2.2_Cross/prompt_match.py
@@ -4,8 +4,8 @@ import difflib
 import json
 
 # === 配置路径 ===
-prompt_txt = "/root/ultrawan/Wan2.2/prompt2.txt"                   # 每行一个 prompt
-video_dir  = "/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/outputs_ultra/480p_base/240p_woman_tx/pt_1"        # 存放 .mp4 或 .pt 文件的目录
+prompt_txt = "/root/ultrawan/Wan2.2/prompt4.txt"                   # 每行一个 prompt
+video_dir  = "/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/outputs_ultra/480p_base/240p_niren/pt_1"        # 存放 .mp4 或 .pt 文件的目录
 
 
 pat = re.compile(r"^ti2v-5B_448\*256_6_(?:\+?prompt_)?(?P<frag>.+?)_\d{8}_\d{6}\.(?:pt|mp4)$")
@@ -47,7 +47,7 @@ for p in prompts:
             "file": os.path.join(video_dir, exact)
         })
 
-out_json = "/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/prompts/woman_blue_240p_pt.json"
+out_json = "/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/prompts/niren_240p_pt.json"
 with open(out_json, "w", encoding="utf-8") as f:
     json.dump(mapping, f, indent=2, ensure_ascii=False)
 
