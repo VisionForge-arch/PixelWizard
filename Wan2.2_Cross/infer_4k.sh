89bd4818791e199acd58ed6616a80b90c8a28a21
李文雪 <liwenxue@xiaomi.com>
2026-01-18 19:43:29 +0800

    m

--- Wan2.2_Cross/infer_4k.sh
+++ Wan2.2_Cross/infer_4k.sh
@@ -47,7 +47,7 @@ torchrun --standalone --nproc_per_node=6 \
     --sample_steps=4 \
     --prompt_file=/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/prompts/eval_100_match_240_new.json \
     --wan_ckpt=/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/Ultra_Train_Weight/4k_shortcut_new2/checkpoint_model_001150/model.pt \
-    --save_file=/mnt/nas01-ak/IndividualDirs/wenxueli/eval_vbench/4k_shortcut_100_new/pt \
+    --save_file=/mnt/nas01-ak/IndividualDirs/wenxueli/eval_100/4k_shortcut_100_new/pt \
     --frame_num=121 \
     --sample_shift=5.8 \
     --dit_fsdp \
