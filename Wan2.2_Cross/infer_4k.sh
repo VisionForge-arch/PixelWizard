94a3771529c4ffb91956e3d82bcc70bd2245d201
李文雪 <liwenxue@xiaomi.com>
2026-01-21 20:55:00 +0800

    m

--- Wan2.2_Cross/infer_4k.sh
+++ Wan2.2_Cross/infer_4k.sh
@@ -44,7 +44,7 @@ export CUDA_VISIBLE_DEVICES=1,2,3,4,5,6
 torchrun --standalone --nproc_per_node=6 \
     generate_multiple_upsample_shortcut.py \
     --size=3840*2144 \
-    --sample_steps=4 \
+    --sample_steps=3 \
     --prompt_file=/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/prompts/eval_100_match_240_new.json \
     --wan_ckpt=/mnt/vision-gen-ks3/IndividualDirs/zp/wenxueli/Output/Ultra_Train_Weight/4k_shortcut_new2/checkpoint_model_001150/model.pt \
     --save_file=/mnt/nas01-ak/IndividualDirs/wenxueli/eval_100/4k_shortcut_100_new/pt_3step \
